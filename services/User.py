148b41ac75d8371294832edbd1ae6f29f46d1c15
Caleb Hester <naclcaleb@gmail.com>
2019-12-20 09:42:16 -0800

    Remove print statements

--- services/User.py
+++ services/User.py
@@ -31,8 +31,6 @@ class User:
         cursor.execute("SELECT interests.name FROM user_interests INNER JOIN interests ON interests.interest_id = user_interests.interest WHERE uid='" + self.uid + "';")
 
         interests = cursor.fetchall()
-        print("SELECT interests.name FROM user_interests INNER JOIN interests ON interests.interest_id = user_interests.interest WHERE uid='{}';".format(self.uid))
-        print(interests)
 
         #Commit and close the connection
         conn.commit()
